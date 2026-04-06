b880fe92aa3bcf7214353e0c2f46ac687fc9b3ef
kabirrifaitokaigiken <kabir_rifai@tokaigiken.co.jp>
2026-04-07 02:27:37 +0700

    bug register and login

--- prepaid_project/settings.py
+++ prepaid_project/settings.py
@@ -64,15 +64,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 
 ROOT_URLCONF = 'prepaid_project.urls'
 
@@ -140,7 +131,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 
 MEDIA_URL = '/media/'
